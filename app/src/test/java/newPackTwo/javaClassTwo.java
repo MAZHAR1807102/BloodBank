110be37d57ef36a32af45a7ddb78f57f9369325a
MAZHAR1807102 <imazharul175@gmail.com>
2022-05-10 21:16:49 +0600

    javaClassTwo

---
+++ app/src/test/java/newPackTwo/javaClassTwo.java
@@ -0,0 +1,26 @@
+package newPackTwo;
+public class javaClassTwo {
+    int integer;
+    String Str;
+
+    public javaClassTwo(int integer, String str) {
+        this.integer = integer;
+        Str = str;
+    }
+
+    public int getInteger() {
+        return integer;
+    }
+
+    public void setInteger(int integer) {
+        this.integer = integer;
+    }
+
+    public String getStr() {
+        return Str;
+    }
+
+    public void setStr(String str) {
+        Str = str;
+    }
+}
