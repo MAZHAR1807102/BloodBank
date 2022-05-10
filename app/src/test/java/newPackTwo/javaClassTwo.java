2339946b3daeb4f2f6f1d566eae863d6f14bf123
MAZHAR1807102 <imazharul175@gmail.com>
2022-05-10 21:46:02 +0600

    again modified

--- app/src/test/java/newPackTwo/javaClassTwo.java
+++ app/src/test/java/newPackTwo/javaClassTwo.java
@@ -1,8 +1,8 @@
 package newPackTwo;
 public class javaClassTwo {
     int integer;
-    float f1;
-    String S1;
+   String S2;
+
 
     public javaClassTwo(int integer, float str) {
         this.integer = integer;
@@ -13,23 +13,15 @@ public class javaClassTwo {
         return integer;
     }
 
-    public String getS1() {
-        return S1;
-    }
-
-    public void setS1(String s1) {
-        S1 = s1;
-    }
-
     public void setInteger(int integer) {
         this.integer = integer;
     }
 
-    public float getStr() {
-        return Str;
+    public String getS2() {
+        return S2;
     }
 
-    public void setStr(String str) {
-        Str = str;
+    public void setS2(String s2) {
+        S2 = s2;
     }
 }
